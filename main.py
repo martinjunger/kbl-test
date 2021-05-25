1a48f7b6817a4618e3ea4bd3d0ab903790f29f35
Martin Junger <martin.junger@gmail.com>
2021-05-25 22:12:00 +0200

    configuration

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 import csv
+import os
+
+# Load the Component library to process the config file
+from keboola.component import CommonInterface
+
+# Rely on the KBC_DATADIR environment variable by default,
+# alternatively provide a data folder path in the constructor (CommonInterface('data'))
+ci = CommonInterface()
+params = ci.configuration.parameters
 
 print("Hello world from python")
 
 csvlt = '\n'
 csvdel = ','
 csvquo = '"'
-with open('in/tables/bitcoin-price.csv', mode='rt', encoding='utf-8') as in_file, \
-        open('out/tables/odd.csv', mode='wt', encoding='utf-8') as odd_file, \
-        open('out/tables/even.csv', mode='wt', encoding='utf-8') as even_file:
+
+# get input table definition by name
+in_table = ci.get_input_table_definition_by_name('bitcoin-price.csv')
+
+with open(in_table.full_path, mode='rt', encoding='utf-8') as in_file, \
+        open(os.path.join(ci.tables_out_path, 'odd.csv'), mode='wt', encoding='utf-8') as odd_file, \
+        open(os.path.join(ci.tables_out_path, 'even.csv'), mode='wt', encoding='utf-8') as even_file:
     lazy_lines = (line.replace('\0', '') for line in in_file)
     reader = csv.DictReader(lazy_lines, lineterminator=csvlt, delimiter=csvdel,
                             quotechar=csvquo)
 
-    even_writer = csv.DictWriter(odd_file, fieldnames=reader.fieldnames,
-                                 lineterminator=csvlt, delimiter=csvdel,
-                                 quotechar=csvquo)
-    even_writer.writeheader()
-
-    odd_writer = csv.DictWriter(even_file, fieldnames=reader.fieldnames,
+    odd_writer = csv.DictWriter(odd_file, fieldnames=reader.fieldnames,
                                 lineterminator=csvlt, delimiter=csvdel,
                                 quotechar=csvquo)
     odd_writer.writeheader()
+
+    even_writer = csv.DictWriter(even_file, fieldnames=reader.fieldnames,
+                                 lineterminator=csvlt, delimiter=csvdel,
+                                 quotechar=csvquo)
+    even_writer.writeheader()
     i = 0
     for row in reader:
         if i % 2 == 0:
             even_writer.writerow(row)
         else:
-            odd_writer.writerow(row)
+            newRow = {}
+            for key in reader.fieldnames:
+                newRow[key] = row[key] + ''.join([params['sound']] * params['repeat'])
+            odd_writer.writerow(newRow)
         i = i + 1
